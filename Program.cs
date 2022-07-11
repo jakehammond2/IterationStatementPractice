da2a7f23afb588ede6438e5c6eda69642e78ebcf
Jake Hammond <jakehammond226@gmail.com>
2022-07-11 11:09:59 -0500

    practice

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿
+Exercise 1
+
+for (int = 0; int < 5; int++)
+{
+    Console.WriteLine($"{i}");
+}
+
+//for (int = 0; int < 5; int++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 4; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 3; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 7; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 6; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 1; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 4; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 11; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 12; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 14; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//for (int i = 0; i < 20; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
